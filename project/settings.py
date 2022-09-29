2afbdc22c8bdf17e6ed796087d1bd19d887d9eaf
Alshefaa Project <elprofessor20121@gmail.com>
2022-09-29 04:19:18 +0200

    Fix no such Table

--- project/settings.py
+++ project/settings.py
@@ -168,3 +168,6 @@ LOGOUT_REDIRECT_URL = "bloodbanks"
 LOGIN_URL = 'login'
 
 django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
